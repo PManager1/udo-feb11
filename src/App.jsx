e0e9345edecb7c18a7048e88d094558975b41e5f
Jay <jpca999@gmail.com>
2026-04-29 14:28:51 -0400

    created otp page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
+import VerifyOtpPage from './pages/VerifyOtpPage'
 import SettingsPage from './pages/SettingsPage'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login/" element={<LoginPage />} />
         <Route path="/signup/" element={<SignupPage />} />
+        <Route path="/verifyOtp" element={<VerifyOtpPage />} />
         <Route path="/settings/" element={<SettingsPage />} />
       </Routes>
     </div>
